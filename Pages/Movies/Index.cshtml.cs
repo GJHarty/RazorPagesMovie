0c791e488dd218ccc97c7cb7925d87c32dd51e31
GJHarty <gharty@live.com>
2022-01-07 15:39:43 -0600

    Added searchstring functionality

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
@@ -21,6 +22,11 @@ namespace RazorPagesMovie.Pages.Movies
         }
 
         public IList<Movie> Movie { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString {  get; set; }
+        public SelectList Genres { get; set; }  
+        [BindProperty(SupportsGet =true)]
+        public string MovieGenre { get; set;}
 
         public async Task OnGetAsync()
         {
