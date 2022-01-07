0f055db1e98ba24b53376400a45ef16aeedf0c86
GJHarty <gharty@live.com>
2022-01-07 16:17:03 -0600

    Added search title functionality

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -25,12 +25,20 @@ namespace RazorPagesMovie.Pages.Movies
         [BindProperty(SupportsGet = true)]
         public string SearchString {  get; set; }
         public SelectList Genres { get; set; }  
-        [BindProperty(SupportsGet =true)]
+        [BindProperty(SupportsGet = true)]
         public string MovieGenre { get; set;}
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            // using System.Linq
+            var movies = from m in _context.Movie 
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            Movie = await movies.ToListAsync();
         }
     }
 }
